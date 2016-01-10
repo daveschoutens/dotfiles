321a991c8e378c3ad1b8245ddd3c50c6776ab9df
Dave Schoutens <dschoutens@gmail.com>
2016-01-09 20:38:25 -0500

    Adding .gitignore. Updating install script

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+## Install .vimrc
+#Don't do this until we can verify the .vimrc is/isn't ours.  Should use version info in the file itself
+#pushd ~
+#mv .vimrc .vimrc.backup
+# Should use dynamically determined dirname...
+#ln -s dotfiles/.vimrc
+#popd
+
+# Install Vundle package
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Use Vundle to install all other plugins
 vim +PluginInstall +qall
 
